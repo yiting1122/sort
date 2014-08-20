5664a34ece2276c1604507e82e7b3da01f00ecac
w_yiting <w_yiting@kingsoft.com>
2014-08-20 21:47:40 +0800

    update

---
+++ suanfa/src/com/suanfa/paixu/SelectSort.java
@@ -0,0 +1,31 @@
+package com.suanfa.paixu;
+
+public class SelectSort {
+
+	public static void main(String[] args) {
+		int a[] = { 1, 54, 6, 3, 78, 34, 12, 45, 56, 100 };
+		a = selectSort(a);
+		for (int i : a) {
+			System.out.println(i);
+		}
+	}
+
+	public static int[] selectSort(int a[]) {
+		for (int i = 0; i < a.length; i++) {
+			int temp = a[i];
+			int pos = i;
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[j] < temp) {
+					temp = a[j];
+					pos = j;
+				}
+			}
+				a[pos] = a[i];
+				a[i] = temp;
+
+
+		}
+		return a;
+	}
+
+}
